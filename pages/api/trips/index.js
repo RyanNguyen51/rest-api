7b204adf238b3ca93b6c660f4632ddf80fdc99af
RyanNguyen51 <Ryannguyen51@hotmail.com>
2022-08-02 20:52:35 -0700

    identification

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -5,5 +5,5 @@ export default async function handler(req, res) {
         return
     }
     const trips = await prisma.trip.findMany()
-    res.status(200).json({ test: 'TRIPS '})
+    res.status(200).json(trips)
 }

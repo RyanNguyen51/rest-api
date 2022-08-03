024de47b79cee3a3e257ab8888ca2b5c6fc89c0e
RyanNguyen51 <Ryannguyen51@hotmail.com>
2022-08-02 20:16:20 -0700

    import files commit

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,9 +1,9 @@
-export default function handler (req, res) {
-    if (req.method !== 'GET') {
-        res.status(405).json({
-            message: 'Method Not Allowed'
-        })
+import prisma from 'lib/prisma'
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET') {res.status(405).send({message: 'Method Not Allowed'})
         return
     }
+    const trips = await prisma.trip.findMany()
     res.status(200).json({ test: 'TRIPS '})
 }
--- pages/index.js
+++ pages/index.js
@@ -172,3 +172,4 @@ const endpoints = [
     </div>
   )
 }
+
